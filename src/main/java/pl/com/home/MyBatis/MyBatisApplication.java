83cfd04638c138fc6ce7c59da0221d4b5053ce3f
Marek Szweda <mszweda@kbj.com.pl>
2020-07-29 12:04:50 +0200

    Inicjalizacja projektu spring initializr.
    Zależności:
    Spring Web        - do napisania żądań http
    Spring Sql Driver - sterownik do bazy danych
    Spring MyBatis

---
+++ src/main/java/pl/com/home/MyBatis/MyBatisApplication.java
@@ -0,0 +1,13 @@
+package pl.com.home.MyBatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBatisApplication.class, args);
+	}
+
+}
